40acdcde85bb7ea88790ab852d5a3ba76a738e17
jontehruss <johann.russeau@protonmail.com>
2024-10-11 14:59:57 +0200

    update structure

--- ReadMe.md
+++ ReadMe.md
@@ -2,3 +2,17 @@
 npx create-react-app kasa
 cd kasa
 npm start
+
+
+2. Installer le compilateur Sass
+npm install -g sass
+
+3. Structure de l'app
+├───assets
+│   └─── Images et fichiers
+├───components
+│   └─── Composants fonctionnels
+├───datas
+│   └─── Fichier de données au format JSON pour simuler une API
+└───styles
+   └─── Fichiers de style par composant
--- kasa/src/components/App.jsx
+++ kasa/src/components/App.jsx
@@ -1,7 +1,8 @@
-import logo from './logo.svg';
-import logo2 from './logo-footer.svg'
-import TopBannerImg from './images/background.jfif';
-import './App.css';
+import logo from "../assets/logo.svg";
+import logoFooter from "../assets/logo-footer.svg";
+import TopBannerImg from "../assets/background.jfif";
+import testDatas from "../datas/logements.json";
+import "../styles/App.css";
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <Footer />
     </div>
   );
-};
+}
 
 function Header() {
   return (
@@ -20,64 +21,87 @@ function Header() {
       <Nav />
     </header>
   );
-};
+}
 
 function Nav() {
   return (
-    <nav >
+    <nav>
       <ul className="Nav-items">
         <li>Accueil</li>
         <li>À Propos</li>
       </ul>
     </nav>
   );
-};
-
+}
 
 function Main() {
   return (
     <main className="App-main">
       {/* Voir s'il faut laisser le style dans le js plutôt que dans le css */}
       <div
-        className='Top-pic-banner'
-        style={{ backgroundImage: `url(${TopBannerImg})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
+        className="Top-pic-banner"
+        style={{
+          backgroundImage: `url(${TopBannerImg})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
       >
         <h1>Chez vous, partout et ailleurs</h1>
       </div>
       <SectionCatalog />
     </main>
   );
-};
+}
 
 function SectionCatalog() {
   return (
-    <section className='App-catalog'>
-      <ItemCard />      
+    <section className="App-catalog">
+      <TestDatas />
+      <ItemCard />
+      <ItemCard />
+      <ItemCard />
+      <ItemCard />
       <ItemCard />
       <ItemCard />
     </section>
   );
-};
+}
+
+console.log(testDatas);
+
+function TestDatas() {
+  return (
+    <ul>
+      {testDatas.map((logement) => (
+        <li>
+          <div>
+            <img src={logement.cover} alt={logement.description} className="Img-item-card"></img>
+            <h2>{logement.title}</h2>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
 
 function ItemCard() {
   return (
-    <div className='Item-card'>
+    <div className="Item-card">
       <picture>
         <span>Titre de la location</span>
       </picture>
     </div>
   );
-};
+}
 
 function Footer() {
   return (
-    <footer className='App-footer'>
-      <img src={logo2} className="Footer-logo" alt="logo" />
+    <footer className="App-footer">
+      <img src={logoFooter} className="Footer-logo" alt="logo" />
       <picture></picture>
       <p>© 2020 Kasa. All rights reserved</p>
     </footer>
   );
-};
-
+}
 
 export default App;
